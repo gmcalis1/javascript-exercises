00761d5c5e2f42882d6f4194a609b1c23f8220ce
Gabriel McAlister <gmcalister88@gmail.com>
2023-02-02 16:16:46 -0500

    Refresh files

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let answer = '';
+    
+    for(i = string.length + 1; i > 0; i--){
+        answer = answer + string.substring(i, i-1);
+    }
+    return answer;
 };
 
 // Do not edit below this line
